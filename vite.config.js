0db1ad2d13d7dbd47b05989cc909b2f76d0f139c
Tyler Marcott <tylerjmarcott@gmail.com>
2023-11-28 16:01:20 -0700

    vite config change

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/FirstLandingPage',
+  base: '/',
   plugins: [vue()],
   build: {
     outDir: 'docs',
